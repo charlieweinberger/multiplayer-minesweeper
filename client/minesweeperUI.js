55d18015865155a8d2d7ebbe04e99fd44458812c
Charlie Weinberger <364charliebrown@gmail.com>
2022-11-19 03:00:36 +0000

    checkpoint on square borders

--- client/minesweeperUI.js
+++ client/minesweeperUI.js
@@ -89,8 +89,84 @@ function updateUI(board, boardSize, status, youWin) {
         }
     }
 
+    for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize; j++) {
+            if (boardHTML.rows[i].cells[j].innerHTML != '') {
+
+                if (i > 0 && j > 0 && elemInList('hidden', boardHTML.rows[i - 1].cells[j - 1].classList)) {
+                    statusHTML.innerHTML = `(i, j): (${i}, ${j})`;
+                    // boardHTML.rows[i].cells[j].background = 'linear-gradient(to top, #87af3a 5px, transparent 5px) 100% 100%;';
+                }
+
+                // background:
+                //      linear-gradient(to bottom, #87af3a 5px, transparent 5px) 0 0,
+                //      linear-gradient(to bottom, #87af3a 5px, transparent 5px) 100% 0,
+                //      linear-gradient(to top, #87af3a 5px, transparent 5px) 0 100%,
+                //      linear-gradient(to top, #87af3a 5px, transparent 5px) 100% 100%;
+
+                if (i > 0 && elemInList('hidden', boardHTML.rows[i - 1].cells[j].classList)) {
+                    boardHTML.rows[i].cells[j].style.borderTop = '5px solid #87af3a';
+                } else {
+                    boardHTML.rows[i].cells[j].style.borderTop = '';
+                }
+
+                // if (i > 0 && j < boardSize - 1 && elemInList('hidden', boardHTML.rows[i - 1].cells[j + 1].classList)) {
+                //     boardHTML.rows[i].cells[j].style.background += 'linear-gradient(to bottom, #87af3a 5px, transparent 5px) 100% 0;';
+                // }
+
+                if (j > 0 && elemInList('hidden', boardHTML.rows[i].cells[j - 1].classList)) {
+                    boardHTML.rows[i].cells[j].style.borderLeft = '5px solid #87af3a';
+                } else {
+                    boardHTML.rows[i].cells[j].style.borderLeft = '';
+                }
+
+                if (j < boardSize - 1 && elemInList('hidden', boardHTML.rows[i].cells[j + 1].classList)) {
+                    boardHTML.rows[i].cells[j].style.borderRight = '5px solid #87af3a';
+                } else {
+                    boardHTML.rows[i].cells[j].style.borderRight = '';
+                }
+
+                // if (i < boardSize - 1 && j > 0 && elemInList('hidden', boardHTML.rows[i + 1].cells[j - 1].classList)) {
+                //     boardHTML.rows[i].cells[j].style.background += 'linear-gradient(to top, #87af3a 5px, transparent 5px) 0 100%;';
+                // }
+
+                if (i < boardSize - 1 && elemInList('hidden', boardHTML.rows[i + 1].cells[j].classList)) {
+                    boardHTML.rows[i].cells[j].style.borderBottom = '5px solid #87af3a';
+                } else {
+                    boardHTML.rows[i].cells[j].style.borderBottom = '';
+                }
+
+                // if (i < boardSize - 1 && j < boardSize - 1 && elemInList('hidden', boardHTML.rows[i + 1].cells[j + 1].classList)) {
+                //     boardHTML.rows[i].cells[j].style.background += 'linear-gradient(to top, #87af3a 5px, transparent 5px) 100% 100%;';
+                // }
+
+            }
+        }
+    }
+
     if (status == 'game over') {
         statusHTML.innerHTML = youWin ? 'You Win!' : 'You Lose!';
     }
 
+}
+
+function getSurroundingCoords(x, y, boardSize) {
+
+    let allPossibleCoords = [
+        [x-1, y-1], [x-1, y], [x-1, y+1], 
+        [x,   y-1], [x,   y], [x,   y+1], 
+        [x+1, y-1], [x+1, y], [x+1, y+1]
+    ];
+
+    return allPossibleCoords.filter(c => 0 <= c[0] && c[0] <= boardSize - 1 && 0 <= c[1] && c[1] <= boardSize - 1);
+
+}
+
+function elemInList(elem, list) {
+    for (let listElem of list) {
+        if (elem === listElem) {
+            return true;
+        }
+    }
+    return false;
 }
